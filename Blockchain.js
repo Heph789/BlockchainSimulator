9fb464dd64407dd6baf80741425416863594df4b
Chase Jeter <chaselbjeter@gmail.com>
2019-04-22 10:44:39 -0400

    Added: 1. Added more realistic transactions having to reference outputs 2. Created a wallet class with elliptic curve addresses To Do: 1. Multiple ledgers that form a blockchain 2. Miners perform mining actions 3. Verification of transactions

--- Blockchain.js
+++ Blockchain.js
@@ -2,15 +2,35 @@
 
 const st = require('./StandardTools.js');
 
+const _pubKey = "036302e5b318785148af1eb7744a4d2cbd5380d4d8faab29b1f80cd7665f066c9e";
+const _privKey = "d287fd14ea6f15635804fa633b6c0ca31598641faa20f50d09d2e147c6106be5";
+
 //@desc The class for a transaction object
 //@param fromAdd The address the transaction is from
 //@param toAdd The address the transaction goes to
 //@param amount Self-explanatory
 class Transaction {
-  constructor(fromAdd, toAdd, amount) {
-    this.fromAdd = fromAdd;
-    this.toAdd = toAdd;
-    this.amount = amount;
+  constructor(inputs, outputs, hash) {
+    this.data = {
+      inputs:inputs,
+      outputs: outputs
+    }
+    this.hash = hash;
+  }
+}
+
+class Input {
+  constructor(previousTx, index, scriptSig) {
+    this.previousTx = previousTx;
+    this.index = index;
+    this.scriptSig = scriptSig;
+  }
+}
+
+class Output {
+  constructor(value, pubKey) {
+    this.value = value;
+    this.pubKey = pubKey;
   }
 }
 
@@ -28,27 +48,35 @@ class BlockData {
 //@param address Address for the starting money
 //@param total Amount of money to start the blockchain with
 class Blockchain {
-  constructor(address, total) {
+  constructor(address, total, eventEmitter) {
+    let outputs = [
+      new Output(total, address)
+    ];
+    let transaction = new Transaction(null, outputs, null);
+    transaction.hash = st.findHash(JSON.stringify(transaction.data));
     let newBlock = new BlockData(
       0,
-      new Transaction(0, address, total),
+      [transaction],
       0
       );
-    this.LEAD = "0000";
+    this.LEAD = "0";
     let _hash = st.findNonce(newBlock, this.LEAD);
-    this.blocks = [{data:newBlock, hash:_hash}];
+    this.blocks = [];
     this.transactions = [];
+    this.eventEmitter = eventEmitter;
+    this.addBlock({data:newBlock, hash:_hash});
   }
 
   addBlock(block) {
     if(block.data instanceof BlockData && block.hash.length>0) {
       this.blocks.push(block);
       this.transactions = [];
+      this.eventEmitter.emit('block', block);
     }
   }
 
-  sendMoney(amount, currentAddress, address) {
-    this.transactions.push(new Transaction(currentAddress, address, amount));
+  broadcastTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
   verify() {
@@ -74,5 +102,8 @@ class Blockchain {
 
 module.exports = {
   Blockchain: Blockchain,
-  BlockData: BlockData
+  BlockData: BlockData,
+  Input: Input,
+  Output: Output,
+  Transaction: Transaction
 }
--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 /*
   Author: Chase Jeter
-  Date: Dec 7, 2018
+  Date: April 2019
   Description: A simulated and interactive proof of work blockchain
 */
+
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
 
 const {Blockchain, BlockData} = require("./Blockchain.js");
 const st = require("./StandardTools.js");
 const fs = require("fs");
 const program = require("commander");
+const Wallet = require("./wallet.js");
 
-var addresses = ["Chase", "Duncan", "Wesley", "Justin"];
-var currentAddress = addresses[0];
+const _pubKey = "036302e5b318785148af1eb7744a4d2cbd5380d4d8faab29b1f80cd7665f066c9e";
+const _privKey = "d287fd14ea6f15635804fa633b6c0ca31598641faa20f50d09d2e147c6106be5";
 
-var blockchain = new Blockchain(currentAddress, 1);
-var obj = JSON.parse(fs.readFileSync("./blockchain.json"));
-for (var prop in obj) {
-  blockchain[prop] = obj[prop];
+
+
+
+var blockchain = new Blockchain("", 1, eventEmitter);
+var data = JSON.parse(fs.readFileSync("./blockchain.json"));
+if(data) {
+  var blockchainObj = data.blockchain;
+  var addresses = data.addresses;
+  for (var prop in blockchainObj) {
+    blockchain[prop] = blockchainObj[prop];
+  }
 }
+blockchain.eventEmitter = eventEmitter;
+
+let wallet = new Wallet([], eventEmitter, blockchain);
+wallet.uploadData("./wallet.json");
 
 function createNewBlockchain(amount) {
   let intAmount = parseInt(amount);
-  blockchain = new Blockchain(currentAddress, intAmount);
+  let address = wallet.createAddress();
+  wallet = new Wallet([address], eventEmitter, null);
+  blockchain = new Blockchain(address.pubKey, intAmount, eventEmitter);
+  wallet.blockchain = blockchain;
 }
 
 function printBlockchain() {
@@ -31,7 +49,7 @@ function printBlockchain() {
 
 function transact(amount, toAddress) {
   let intAmount = parseInt(amount);
-  blockchain.sendMoney(intAmount, currentAddress, toAddress);
+  return wallet.transact(toAddress, intAmount);
 }
 
 function addBlock() {
@@ -94,7 +112,8 @@ program
   .alias('sm')
   .description('sends money to <toAdress> with the amount <amount>')
   .action((toAddress, amount) => {
-    transact(amount, toAddress);
+    if(!transact(amount, toAddress))
+      console.log("Something went wrong");
   });
 
 program
@@ -116,11 +135,43 @@ program
 program
   .command('addCustomBlock <hash> <nonce> <previousHash>')
   .alias('acb')
-  .description('adds a custom block to the blockchain using custom <hash> and <nonce>. <previousHash is supposed to be the hash of the previous block')
+  .description('adds a custom block to the blockchain using custom <hash> and <nonce>. <previousHash> is supposed to be the hash of the previous block')
   .action((hash, nonce, previousHash) => {
     addCustomBlock(hash, nonce, previousHash);
   });
 
-program.parse(process.argv);
+program
+  .command('createAddress')
+  .alias('ca')
+  .description('creates a public-private key pair and prints it out')
+  .action(() => {
+    wallet.createAddress();
+    console.log(wallet.addresses);
+  });
 
-fs.writeFileSync("./blockchain.json", JSON.stringify(blockchain));
+program
+  .command('listAddresses')
+  .alias('la')
+  .description('list all of the addresses in the wallet and the current address')
+  .action(() => {
+    console.log(wallet.addresses);
+    console.log("Current address: ");
+    console.log(wallet.currentAddress);
+  })
+
+program
+  .command('setAddress <index>')
+  .alias('sa')
+  .description('sets the current address to the <index> address with the first being 0')
+  .action((index) => {
+    wallet.setAddress(index);
+    console.log(wallet.currentAddress);
+  })
+
+program.parse(process.argv);
+const obj = {
+  blockchain: blockchain,
+  addresses: addresses
+}
+fs.writeFileSync("./blockchain.json", JSON.stringify(obj));
+wallet.saveData("./wallet.json");
